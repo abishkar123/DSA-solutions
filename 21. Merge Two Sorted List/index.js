91eb03621e5ee3d4e90754ccefbff534f4b97eed
abishkar rai <raiabishkar0.5@gmail.com>
2024-09-17 15:17:26 +1000

    21.Merage two sorted list

---
+++ 21. Merge Two Sorted List/index.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+function mergeTwoLists(l1, l2) {
+    let dummy = new ListNode(-1);  // Create a dummy node to form the result list
+    let current = dummy;
+
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+
+    current.next = l1 ? l1 : l2;  
+    return dummy.next;
+}
