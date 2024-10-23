34e35133d6b8bdddc4ede3c00340d5db4d7d2833
abishkar rai <raiabishkar0.5@gmail.com>
2024-10-24 09:59:54 +1100

    more dsa

---
+++ 215. Kth Largest Element in an Array/index.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+function findKthLargest(nums, k) {
+    k = nums.length - k;
+  
+    function quickSelect(l, r) {
+      let pivot = nums[r], p = l;
+      for (let i = l; i < r; i++) {
+        if (nums[i] <= pivot) {
+          [nums[p], nums[i]] = [nums[i], nums[p]];
+          p++;
+        }
+      }
+      [nums[p], nums[r]] = [nums[r], nums[p]];
+  
+      if (p > k) {
+        return quickSelect(l, p - 1);
+      } else if (p < k) {
+        return quickSelect(p + 1, r);
+      } else {
+        return nums[p];
+      }
+    }
+  
+    return quickSelect(0, nums.length - 1);
+  }
+  
