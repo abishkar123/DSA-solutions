ecb9f3052426b768f3da18cd732579bcb5a336a5
abishkar rai <raiabishkar0.5@gmail.com>
2024-11-12 12:19:23 +1100

    more dsa

--- 138. Copy List with Random Pointer/index.js
+++ 138. Copy List with Random Pointer/index.js
@@ -1,3 +1,16 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {_Node} head
+ * @return {_Node}
+ */
 
 var copyRandomList = function(head) {
     if (!head) return null;
