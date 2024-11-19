f30029e34005f11a4892eb495a6cc880f639dfca
abishkar rai <raiabishkar0.5@gmail.com>
2024-11-19 21:43:32 +1100

    more dsa solutions

---
+++ 278. First Bad Version/index.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+   return function (n) {
+        let low = 1;
+        let high = n;
+        let mid;
+        while (low <= high) {
+            mid = ~~(low + (high - low) / 2);
+            if (isBadVersion(mid)) {
+                if (isBadVersion(mid - 1)) high = mid - 1;
+                else return mid;
+            } else low = mid + 1;
+        }
+    }
+};
