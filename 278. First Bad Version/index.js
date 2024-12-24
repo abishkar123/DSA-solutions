031a0f5cb904a867ff4015c7d29e8fc6dd3285d7
abishkar rai <raiabishkar0.5@gmail.com>
2024-12-24 14:06:53 +1100

    update

--- 278. First Bad Version/index.js
+++ 278. First Bad Version/index.js
@@ -1,17 +1,4 @@
-/**
- * Definition for isBadVersion()
- * 
- * @param {integer} version number
- * @return {boolean} whether the version is bad
- * isBadVersion = function(version) {
- *     ...
- * };
- */
 
-/**
- * @param {function} isBadVersion()
- * @return {function}
- */
 var solution = function(isBadVersion) {
     
    return function (n) {
